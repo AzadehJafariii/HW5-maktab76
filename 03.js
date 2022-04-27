d69894fa44c5ae3616b54b2c1ae60129b7d04002
AzadehJafari <jafari.azadeh2018@gmail.com>
2022-04-28 02:50:48 +0430

    03.js

--- 03.js
+++ 03.js
@@ -2,6 +2,7 @@
 // write a function creates an object with key & value from array 
 // given and add uppercase as values of object: 
 
+
 function makeObject(){
     let array = ['a', 'b', 'c', 'd'];
     let result = array.reduce(function(acc, item){
